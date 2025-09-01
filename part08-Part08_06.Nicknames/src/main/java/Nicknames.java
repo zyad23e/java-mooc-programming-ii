04ac623f09c38b901cd08a33ccdefae44c9c913a
zyad23e <zyade23@arizona.edu>
2025-09-01 11:59:04 -0700

    Progress: Completed more Java MOOC part 8 exercises

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        // Do the operations required here!
+        HashMap<String, String> nickname = new HashMap<>();
+        nickname.put("matt", "matthew");
+        nickname.put("michael", "mix");
+        nickname.put("arthur", "artie");
+    }
+
+}
