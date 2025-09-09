a3e5d99a0b00684f8ef0329061c23833186a715b
zyad23e <zyade23@arizona.edu>
2025-09-09 10:11:31 -0700

    Progress: Completed more Java MOOC part 9 exercises

---
+++ part09-Part09_05.TacoBoxes/src/main/java/CustomTacoBox.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zyade
+ */
+public class CustomTacoBox implements TacoBox{
+    private int tacos;
+    
+    public CustomTacoBox(int tacos){
+        this.tacos = tacos;
+    }
+    
+    public int tacosRemaining(){
+        return this.tacos;
+    }
+    
+    public void eat(){
+        if (tacos - 1 >= 0){
+          tacos -=1;  
+        }
+    }
+    
+}
