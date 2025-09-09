0c262427c740f8379dd4abc5b09d79d4463be9ce
zyad23e <zyade23@arizona.edu>
2025-09-09 14:45:29 -0700

    Progress: Completed more Java MOOC part 9 exercises

---
+++ part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -0,0 +1,20 @@
+
+import java.util.List;
+
+
+
+public class mainProgram {
+
+    public static void main(String[] args) {
+        // test your method here
+
+    }
+    
+    public static int returnSize(List<String> items){
+        return items.size();
+    }
+
+    // Implement here a method returnSize
+    // which returns the size of the list given to it
+    //as a parameter
+}
