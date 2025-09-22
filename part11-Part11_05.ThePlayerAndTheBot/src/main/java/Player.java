d62e84d4c53e0c2c58138634487e7988c91d4191
zyad23e <zyade23@arizona.edu>
2025-09-22 10:50:39 -0700

    Progress: Completed more Java MOOC part 11 exercises

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zyade
+ */
+public class Player{
+    private String name;
+    
+    public void play(){
+        
+    }
+    
+    public void printName(){
+        System.out.println(this.name);
+    }
+    
+}
